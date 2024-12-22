e7d68420f50ad940b753385c49d188f3db1843ae
derinritter <derinaritter@protonmail.com>
2024-12-22 06:08:52 +0300

    refactor aad

--- mcmunsite/layouts/base.vue
+++ mcmunsite/layouts/base.vue
@@ -11,7 +11,7 @@
 <script lang="ts" setup>
 const pageSecurityIsEnabled = useRuntimeConfig().public.pageSecurityIsEnabled;
 
-if (pageSecurityIsEnabled) {
+if (pageSecurityIsEnabled === "1") {
   disableContextMenu();
   disableDevTools();
 }
